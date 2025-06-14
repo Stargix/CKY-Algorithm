3a11866568ce7617265e5e6878b9059402dd77c4
Stargix <11sergifm@gmail.com>
2025-06-14 11:40:29 +0200

    puterito

---
+++ main.py
@@ -0,0 +1,8 @@
+
+class empicaelcul():
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def calculate_salary(self):
+        return self.salary * 1.2  # Example calculation with a 20% increase
